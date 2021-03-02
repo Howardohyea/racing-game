4973a399f333a5c52e379f47eb5ed0533666d9ab
Howardohyea <howardohyea@outlook.com>
2021-03-02 22:04:48 +0800

    broken state, reverting

--- Assets/Code/TrackSelectMenu.cs
+++ Assets/Code/TrackSelectMenu.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class TrackSelectMenu : MonoBehaviour
 {
+    bool bullshit
 
     public void PlayTrack1()
     {
         SceneManager.LoadScene(1);
+        bullshit = true;
 
+    }
+    public void Update()
+    {
+        bullshit = false;
+        //just make sure everything related to bullshit is removed during the final coding session
     }
 
     public void Quitgame()
